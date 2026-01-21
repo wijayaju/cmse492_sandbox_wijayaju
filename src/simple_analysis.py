08f99c1784801b0a497bd18facb58e7bcaa13c45
Justin <wijayaju@msu.edu>
2026-01-21 12:21:18 -0500

    Add seaborn to conda environment and update analysis

--- src/simple_analysis.py
+++ src/simple_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.DataFrame({'a':[1,2,3,4,5], 'b':[6,7,8,9,10], 'c':[11,12,13,14,15]})
 
@@ -12,4 +13,12 @@ plt.title('b vs c')
 plt.xlabel('a')
 plt.ylabel('values')
 plt.legend()
+plt.show()
+
+# Create sample data
+data = np.random.randn(100)
+
+# Create a seaborn histogram
+sns.histplot(data, kde=True)
+plt.title("Sample Distribution")
 plt.show()
